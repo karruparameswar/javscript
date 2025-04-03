96c1ce693658941fe842c49ca4fc056216313bba
karruparameswar <104783516+karruparameswar@users.noreply.github.com>
2025-04-03 10:18:11 +0000

    dataypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="karrruparameswar@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+accountEmail="hc@hc.com"
+let accountState;
+accountPassword="212121"
+accountCity="bangloe"
+console.log(accountId);
+/*
+prefered not to use var because of the use in the  blockscope and functional scope
+*/
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//alert("hello")// we are using the node js not browser
+console.log(3
+    +3); 
+//code readability should be high
+console.log("param")
+let name="param"
+let age=18
+let isLoggedIn=false
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=standalonevalue
+//undefined=>
+//symbol=>unique
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
